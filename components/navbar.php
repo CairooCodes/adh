4696e91fcb9966c9b17000f8557405229fc8d0e9
Alex-Brendon <alexbrendon11.dev@gmail.com>
2023-07-05 12:42:20 -0300

    ajust footer and navbar, add img perfil adh

---
+++ components/navbar.php
@@ -0,0 +1,54 @@
+<header class="w-full shadow fixed z-10 bg-white">
+    <nav class="border-gray-200 py-5" x-data="{ show: true }">
+      <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+        <a href="#" class="flex items-center">
+
+          <img src="./assets/img/logo.jpg" class="w-60" alt="ADH" />
+        </a>
+        <div class="flex items-center lg:order-2 space-x-2">
+          <a href="#" class="hidden lg:block text-white bg-color1 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0">Registre-se
+          </a>
+          <div id="theme_toggler">
+            <i @click="show = !show" :class="{'hidden': !show, 'block':show }" class="bi bi-moon-stars-fill text-color2"></i>
+            <i @click="show = !show" :class="{'block': !show, 'hidden':show }" class="bi text-color1 bi-brightness-high-fill"></i>
+          </div>
+          <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+            </svg>
+            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+            </svg>
+          </button>
+        </div>
+        <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+          <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 bg-color1 rounded lg:bg-transparent lg:text-color1 lg:p-0" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:text-color1 lg:p-0">Cursos</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 lg:bg-transparent lg:border-0 lg:text-color1 lg:p-0">PodCast</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 lg:bg-transparent lg:border-0 lg:text-color1 lg:p-0">E-Book</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 lg:bg-transparent lg:border-0 lg:text-color1 lg:p-0">Quem
+                Somos</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <a data-modal-target="addUserModal" data-modal-show="addUserModal" class="pointer">
+    <div class="btn_whats">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+        <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+      </svg>
+    </div>
+  </a>

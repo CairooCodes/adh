bcf10b4b90806e874143c28a23eacaefdb654d97
Cairo <cairofelipedev@gmail.com>
2023-08-10 11:52:34 -0300

    Ajust lp

--- landingpage.php
+++ landingpage.php
@@ -35,81 +35,73 @@
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/intersection-observer@0.12.2/intersection-observer.min.js"></script>
+  <style>
+    /* Definindo a classe para aplicar o efeito fade-in-up */
+    .fade-in-up {
+      opacity: 0;
+      transform: translateY(20px);
+      transition: opacity 0.5s ease-out, transform 0.5s ease-out;
+    }
+
+    .fade-in-up.visible {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  </style>
 </head>
 
-<body class="">
-  <?php // include "./components/navbar.php"; 
-  ?>
-
-  <section class="px-5 lg:py-24 py-12 lg:px-32 bg-gradient-to-r from-black from-35% via-gray-600 via-100%">
-    <div class="lg:flex lg:justify-center">
-      <div class="lg:w-2/2">
-        <div class="flex justify-center">
-          <img src="./assets/img/logo2.png" width="500px" alt="ADH" />
-        </div>
-        <h3 class="mb-4 lg:text-4xl text-2xl font-bold lg:mt-10 text-gray-100 text-center"><span class="text-color1">ADH MED - </span>Abordagens Modernas em Medicina Cardiovascular.
-        </h3>
-        <p class="text-md font-medium text-white md:text-lg mb-28 text-justify">
-          Descubra o fascinante mundo da Medicina Cardiovascular, onde a inovação encontra o cuidado ao paciente! Desvende o potencial de diagnósticos avançados e tratamentos personalizados para vencer as doenças cardíacas. De terapias genéticas pioneiras a intervenções assistidas por inteligência artificial de última geração, testemunhe uma revolução médica que salva vidas como nunca antes. Abra espaço para a medicina de precisão, adaptada às necessidades individuais, aproveitando o poder da genômica e proteômica para desbloquear terapias personalizadas. Viva o futuro com técnicas não invasivas, maravilhas da nanotecnologia e terapias regenerativas, moldando uma nova era nos cuidados cardíacos. Junte-se a nós nessa jornada épica de esperança, reescrevendo a história da saúde cardiovascular e elevando o bem-estar da humanidade a alturas surpreendentes!"
-        </p>
-
-        <button class="w-full bg-white border rounded py-2 mb-5 font-semibold text-2xl hover:bg-color1 hover:text-gray-200">
-          GARANTA SUA VAGA E DOMINE A MEDICINA CARDIOVASCULAR!
-        </button>
-        <p class="text-gray-100 text-center mb-32">
-          Nos dias 10, 11 e 12 de Agosto <span class="text-color2 text-xs"> EM </span> Teresina
-        </p>
-  </section>
-  <section class="px-5 lg:py-24 py-12 lg:px-32">
-    <div class="items-center gap-8 lg:grid lg:grid-cols-3 xl:gap-8 text-white">
-      <div class="bg-color1 rounded-lg px-5 py-5 m-2">
-        <div class="flex flex-nowrap">
-          <i class="text-6xl bi bi-heart-pulse"></i>
-          <p class="text-xl pl-10 font-bold">MÓDULO I</p>
-        </div>
-        <h1 class="text-white pt-4 font-bold text-2xl pb-2">Descubra os Segredos do Coração:</h1>
-        <p class="text-lx text-white text-justify">Explore a anatomia, diagnósticos e tratamentos inovadores para desvendar o fascinante mundo da cardiologia. Aprenda a interpretar o ECG e aprofunde-se nas principais doenças cardíacas.</p>
-      </div>
-      <div class="bg-color1 rounded-lg px-5 py-5 m-2">
-        <div class="flex flex-nowrap">
-          <i class="text-6xl bi bi-clipboard2-pulse"></i>
-          <p class="text-xl pl-10 font-bold">MÓDULO II</p>
-        </div>
-        <h1 class="text-white pt-4 font-bold text-2xl pb-2">Aprofunde-se na Essência do Coração:</h1>
-        <p class="text-lx text-white">Curso Intermediário: Anatomia avançada, diagnósticos especializados e tecnologias inovadoras. Desvende casos clínicos desafiadores e aprofunde-se na fisiologia cardíaca. Torne-se um especialista do coração.</p>
-      </div>
-      <div class="bg-color1 rounded-lg px-5 py-5 m-2">
-        <div class="flex flex-nowrap">
-          <i class="text-6xl bi bi-prescription2"></i>
-          <p class="text-xl pl-10 font-bold">MÓDULO III</p>
-        </div>
-        <h1 class="text-white pt-4 font-bold text-2xl pb-2">Excelência na Essência do Coração:</h1>
-        <p class="text-lx text-white">Curso Avançado de Medicina Cardiovascular: Intervencionismo, tecnologias de imagem e diagnósticos avançados. Aprofunde-se em tratamentos complexos e torne-se uma referência na área.</p>
+<body>
+  <section class="px-5 lg:px-32 lg:pb-40" style="background: rgb(65,43,108);
+background: linear-gradient(0deg, rgba(65,43,108,1) 0%, rgba(65,43,108,1) 25%, rgba(255,255,255,1) 100%);">
+    <div class='loading-container relative'>
+      <div class='loading h-2 w-[0%] bg-green-500 transition-all duration-200 absolute z-40 top-0'></div>
+      <div class="flex justify-center">
+        <span class="progress-text absolute top-2"></span>
       </div>
     </div>
-  </section>
-
-  <section class="p-2 lg:py-16 lg:px-32 bg-no-repeat bg-cover bg-center" style="background-image: url('./assets/img/core1.jpg')">
     <div class="lg:flex lg:justify-center">
-      <div class="lg:w-2/2">
-        <div class="lg:grid lg:grid-cols-2 gap-2">
-          <h3 class="lg:text-4xl text-2xl font-bold lg:mt-48 text-color1 text-center">
-            ACADEMIA ADH,<br><br>
-            você entre os melhores.
+      <div class="absolute lg:top-0 top-10 z-0">
+        <img src="./assets/img/git_adh.gif" alt="ADH" />
+      </div>
+      <div class="lg:w-2/2 flex pt-24">
+        <div class="z-10">
+          <h3 style="text-shadow: 1px 1px white" class="fade-in-up mb-4 lg:text-4xl text-3xl font-bold lg:mt-10 text-center">Como diagnosticar seu paciente com confiança e eficiencia em casos de diabete e hipertesao
           </h3>
-          <p class="text-md font-medium text-color1 md:text-lg mb-2 text-justify">
-            O curso intensivo em medicina cardiovascular aborda tópicos avançados e emergentes para a compreensão das doenças cardíacas. Os participantes terão acesso a recursos educacionais de alta qualidade e interação com especialistas renomados. A ênfase na colaboração multidisciplinar visa melhorar o atendimento aos pacientes.
-            <br><br>
-            A abordagem de aprendizado inclui workshops práticos e simulações clínicas realistas. Além disso, destaca-se a importância da pesquisa inovadora para uma visão holística da medicina cardiovascular.
-            <br><br>
-            O programa incentiva o aprimoramento das habilidades de comunicação e trabalho em equipe, adaptando-se a ambientes de saúde complexos. Estratégias para implementar as melhores práticas na prática clínica são discutidas, visando impactar positivamente um número maior de pacientes e comunidades. Ao concluir o curso, os profissionais estarão preparados para se destacarem na vanguarda da medicina cardiovascular.
-            <button class="w-full mt-10 text-color1 rounded py-2 font-semibold text-xl hover:bg-color1 hover:text-gray-100">QUERO FAZER PARTE DA MINORIA QUE TEM RESULTADO</button>
+          <button class="my-5 w-full lg:mt-20 relative bg-green-500 hover:bg-blue-600 text-white font-semibold py-5 px-4 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300">
+            <span class="animate-bounce absolute inset-x-0 bottom-0 text-center">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mx-auto text-white" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+              </svg>
+            </span>
+            GARANTIR MINHA VAGA
+          </button>
+          <p class="text-center text-white lg:mb-32 mb-10 text-md">
+            Nos dia 2 de setembro <span class="text-color2 text-xs"> EM </span> Teresina
           </p>
         </div>
       </div>
     </div>
   </section>
-
+  <section class="max-w-5xl mx-auto pt-10 pb-4">
+    <h1 class="lg:text-4xl text-3xl font-bold leading-none tracking-tight md:text-5xl xl:text-5xl text-center mb-4">
+      Conheça nosso time
+    </h1>
+    <div class="lg:flex lg:space-x-40 p-5">
+      <div class="lg:mb-4 mb-16 fade-in-up">
+        <img class="rounded-full" src="./assets/img/Dr. Alcinio.jpg" alt="Dr. Alcinio" />
+        <h1 class="mt-4 text-3xl font-bold text-center">Dr. Alcino Sá</h1>
+      </div>
+      <div class="lg:mb-4 mb-16 fade-in-up">
+        <img class="rounded-full" src="./assets/img/Dr. Aécio.jpg" alt="Dr. Aécio" />
+        <h1 class="mt-4 text-3xl font-bold text-center">Dr. Aécio</h1>
+      </div>
+      <div class="lg:mb-4 mb-16 fade-in-up">
+        <img class="rounded-full" src="./assets/img/Dr. Jonatas.jpg" alt="Dr. Jonatas" />
+        <h1 class="mt-4 text-3xl font-bold text-center">Dr. Jonatas</h1>
+      </div>
+    </div>
+  </section>
   <section class="p-5 lg:py-16 lg:px-32 bg-gray-100">
     <div class="lg:flex lg:justify-center">
       <div class="lg:w-2/2">
@@ -134,7 +126,7 @@
     </div>
   </section>
 
-  <div class="pt-10 form bg-no-repeat bg-center bg-cover bg-contain" style="background-image: url('./assets/img/banner-core.jpg')">
+  <div class="pt-10 form bg-no-repeat bg-center bg-cover bg-contain" style="background-image: url('./assets/img/image-teresina-uchoa-hotel-16.jpeg')">
     <div class="lg:w-2/3 pt-20 px-4 pb-8 mx-auto lg:pb-16">
       <div class="shadow-inner bg-white bg-opacity-40 shadow-md rounded-xl border border-gray-300">
         <div class="max-w-screen-xl px-4 pt-8 mx-auto mb-3 text-center">
@@ -189,6 +181,47 @@
   <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
 
+  <script>
+    const loadingContainer = document.querySelector('.loading-container');
+    const loading = loadingContainer.querySelector('.loading');
+    const progressText = loadingContainer.querySelector('.progress-text');
+
+    let currentProgress = 0;
+    let itv = setInterval(function() {
+      if (currentProgress < 100) {
+        const increment = Math.random() * 40;
+        currentProgress += increment;
+        if (currentProgress > 70) currentProgress = 70;
+        setProgress(currentProgress);
+      } else {
+        clearInterval(itv);
+      }
+    }, 800);
+
+    function setProgress(progress) {
+      loading.style.width = `${progress}%`;
+      progressText.textContent = `${Math.round(progress)}% das vagas preenchidas`;
+    }
+  </script>
+  <script>
+    const cards = document.querySelectorAll('.fade-in-up');
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.2
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    cards.forEach(card => observer.observe(card));
+  </script>
 </body>
 
 </html>
